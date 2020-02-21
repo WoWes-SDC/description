725c5c2899e63dc7388deacde736197025fee263
RoyOlivarez <royolivarez66@gmail.com>
2020-02-21 15:15:55 -0600

    Working on adding postgres to config

--- database/config.js
+++ database/config.js
@@ -1,11 +1,22 @@
-  
+/* eslint-disable linebreak-style */
+
+// SDC Config
+module.exports = {
+  host: 'localhost',
+  port: '3002',
+  user: 'postgres',
+  password: 'password',
+  database: 'SDC',
+};
+
+// WoWes Deployed config
 module.exports = {
   host: 'database-2.cid9za1z1dus.us-east-2.rds.amazonaws.com',
   port: '3306',
   user: 'admin',
   password: 'T3%tt#5t',
   database: 'FECdata',
-}
+};
 
 /* LOCAL SETTINGS
 module.exports = {
